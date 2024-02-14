f55d0567037689a6d9387191de15816186389a13
Mahir Nanjib Chowdhury <mahirnanjib166@gmail.com>
2024-02-15 00:02:09 +0600

    added minimum js files and will be fixing furthur

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-    
+//  selection all elements by their class or id or name and storing them in various container
+
+let text = document.querySelector(".text")
+let textDiv = document.querySelector(".textDiv")
+let resetButton = document.querySelector(".reset-Btn")
+let resetDiv = document.querySelector(".resetDiv")
+let bgColor = document.querySelector(".bgColor")
+let fontSize = document.querySelector(".fontSize")
+let selectionDiv = document.querySelector(".selectionDiv")
+let functionHolder = document.querySelector(".functionHolder")
+let motherDiv = document.querySelector(".motherDiv")
